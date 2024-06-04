934a8c80754b1c2c094a469d79cbb21756ab8ba1
titronius <titronius@gmail.com>
2024-06-04 13:14:40 +0300

    delete non used

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ import re
 with open("phonebook_raw.csv", encoding="utf-8") as f:
   rows = csv.reader(f, delimiter=",")
   contacts_list = list(rows)
-# pprint(contacts_list)
 
 # Собираем имена
 for number, contact in enumerate(contacts_list):
@@ -47,10 +46,7 @@ for number, contact in enumerate(clear_contacts_list):
     contact[5] = result
 
 pprint(clear_contacts_list)
-# TODO 1: выполните пункты 1-3 ДЗ
-# ваш код
 
-# TODO 2: сохраните получившиеся данные в другой файл
 # код для записи файла в формате CSV
 with open("phonebook.csv", "w", encoding="utf-8") as f:
   datawriter = csv.writer(f, delimiter=',')
